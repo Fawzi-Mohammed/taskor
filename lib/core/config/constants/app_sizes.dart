9b0fc683d3cf2c60849c5c3658e638cebadfc87e
Fawzi-Mohammed <fawmoh2014@gmail.com>
2025-12-25 17:32:09 +0200

    done app_sting and color and app_size

---
+++ lib/core/config/constants/app_sizes.dart
@@ -0,0 +1,38 @@
+class AppSizes {
+  AppSizes._();
+
+  // =====================
+  // Spacing (Padding & Margin)
+  // =====================
+  static const double s4 = 4;
+  static const double s8 = 8;
+  static const double s12 = 12;
+  static const double s16 = 16;
+  static const double s20 = 20;
+  static const double s24 = 24;
+  static const double s32 = 32;
+  static const double s40 = 40;
+
+  // =====================
+  // Border Radius
+  // =====================
+  static const double r8 = 8;
+  static const double r12 = 12;
+  static const double r16 = 16;
+  static const double r24 = 24;
+
+  // =====================
+  // Button Heights
+  // =====================
+  static const double buttonHeightSmall = 40;
+  static const double buttonHeightMedium = 48;
+  static const double buttonHeightLarge = 56;
+
+  // =====================
+  // Icon Sizes
+  // =====================
+  static const double icon16 = 16;
+  static const double icon20 = 20;
+  static const double icon24 = 24;
+  static const double icon32 = 32;
+}
